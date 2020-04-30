fb724929db9f9ef2104ee7407b7ff5be9a44f673
Nikita <nikita.malewich@gmail.com>
2020-04-30 12:34:01 +0300

    Beginning of the third solution of Task14

--- A&DS_Tasks/A&DS_Task14/main.cpp
+++ A&DS_Tasks/A&DS_Task14/main.cpp
@@ -1,119 +1,75 @@
 #include <fstream>
+#include <queue>
 #include <algorithm>
-#include <vector>
 
-int* list;
-int** arc_list;
-int num_start_node;
-int num_finish_node;
-int length_min_way;
-int length_second_min_way;
-std::vector<std::vector<int>> ways;
-std::vector<std::vector<int>> cycles;
+std::vector<std::pair<int, int>>* adjacency_list;
+int num_start_vertex;
+int num_finish_vertex;
+auto cmp = [](std::pair<int, int> first, std::pair<int, int> second) {
+    return first.second > second.second;
+};
+int* is_check_vertex;
+int* dijkstra_arr;
+int length;
 
-void dfs(std::vector<int>& way, int num_node) {
-    if (num_node == num_finish_node) {
-        if (way[0] < length_min_way) {
-            length_second_min_way = length_min_way;
-            length_min_way = way[0];
-        } else if (way[0] > length_min_way && way[0] < length_second_min_way) {
-            length_second_min_way = way[0];
-        }
+void dijkstra(std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, decltype(cmp)>& queue) {
+    auto cur_pair = queue.top();
 
-        ways.push_back(way);
+    queue.pop();
+    is_check_vertex[cur_pair.first]++;
+
+    if(is_check_vertex[num_finish_vertex] == 2) {
+        length = cur_pair.second;
+
+        return;
     }
 
-    int num_next_arc = list[num_node];
-    int num_next_node = arc_list[num_next_arc][0];
-
-    while (num_next_node != 0) {
-        if (std::find(way.begin(), way.end(), num_next_arc) == way.end()) {
-            std::vector<int> new_way(way);
-
-            new_way.push_back(num_next_arc);
-            new_way[0] += arc_list[num_next_arc][1];
-
-            dfs(new_way, num_next_node);
-        } else {
-            cycles.push_back(way);
+    for (auto& pair : adjacency_list[cur_pair.first]) {
+        if (is_check_vertex[pair.first] != 2) {
+            queue.push(std::pair<int, int>(pair.first, pair.second + cur_pair.second));
         }
-
-        num_next_arc = arc_list[num_next_arc][2];
-        num_next_node = arc_list[num_next_arc][0];
     }
+
+    dijkstra(queue);
 }
 
 int main() {
     std::ifstream in("input.in");
     std::ofstream out("output.out");
 
-    int num_nodes;
+    int num_vertices;
     int num_arcs;
 
-    in >> num_nodes >> num_arcs;
+    in >> num_vertices >> num_arcs;
 
-    list = new int[num_nodes + 1]();
-    arc_list = new int*[num_arcs + 1];
+    adjacency_list = new std::vector<std::pair<int, int>>[num_vertices];
 
-    arc_list[0] = new int[3]();
-
-    for (int i = 1; i <= num_arcs; i++) {
-        int num_node1;
-        int num_node2;
+    for (int i = 0; i < num_arcs; i++) {
+        int num_vertex1;
+        int num_vertex2;
         int weight;
 
-        in >> num_node1 >> num_node2 >> weight;
-        arc_list[i] = new int[3]();
+        in >> num_vertex1 >> num_vertex2 >> weight;
+        num_vertex1--;
+        num_vertex2--;
 
-        arc_list[i][0] = num_node2;
-        arc_list[i][1] = weight;
-        arc_list[i][2] = list[num_node1];
-
-        list[num_node1] = i;
+        adjacency_list[num_vertex1].emplace_back(num_vertex2, weight);
     }
 
-    in >> num_start_node >> num_finish_node;
+    in >> num_start_vertex >> num_finish_vertex;
+    num_start_vertex--;
+    num_finish_vertex--;
 
-    std::vector<int> start_way;
-    length_min_way = INT_MAX;
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, decltype(cmp)> queue(cmp);
+    is_check_vertex = new int[num_vertices]();
+    dijkstra_arr = new int[num_vertices]();
 
-    start_way.push_back(0);
+    queue.push(std::pair<int, int>(num_start_vertex, 0));
+    length = 0;
+    std::fill(dijkstra_arr, dijkstra_arr + num_vertices, INT_MAX);
+    dijkstra_arr[num_start_vertex] = 0;
 
-    dfs(start_way, num_start_node);
-
-    for (std::vector<int>& way : ways) {
-        for (int num_arc : way) {
-            out << num_arc << ' ';
-        }
-
-        out << '\n';
-    }
-
-    out << length_min_way << ' ' << length_second_min_way << '\n';
-
-    for (std::vector<int>& cycle : cycles) {
-        for (int num_arc : cycle) {
-            out << num_arc << ' ';
-        }
-
-        out << '\n';
-    }
-
-//    for (int i = 1; i < num_nodes + 1; i++) {
-//        out << list[i] << ' ';
-//    }
-//
-//    out << '\n';
-//
-//    for (int i = 1; i < num_arcs + 1; i++) {
-//        for (int j = 0; j < 3; j++) {
-//            out << arc_list[i][j] << ' ';
-//        }
-//
-//        out << '\n';
-//    }
-
-
+    dijkstra(queue);
 
     in.close();
     out.close();
